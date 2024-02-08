b476924f34afbfdaccf5a0c07f87a182484df42c
Sergio <gomez.c.sergio@outlook.com>
2024-02-08 16:39:11 +0100

    Add the object classes to the DTO

---
+++ EF_DTO/CalendarEntry.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EF_DTO
+{
+    public class CalendarEntry
+    {
+        [Key]
+        public int Id { get; set; }
+        public virtual Kid Kid { get; set; }
+        public DateTime Date { get; set; }
+        public string Data { get; set; }
+        public bool Validated { get; set; }
+        public virtual Price Price { get; set; }
+    }
+}
---
+++ EF_DTO/Kid.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EF_DTO
+{
+    public class Kid
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Lastname { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime? EndDate { get; set; }
+        public DateTime? Birthdate { get; set; }
+
+
+        public virtual ICollection<SiblingRelationship>? Siblings { get; set; }
+    }
+}
