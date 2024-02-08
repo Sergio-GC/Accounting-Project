c4aaf77b7d5fec334ff77f40032b2da969bb2cfd
Sergio <gomez.c.sergio@outlook.com>
2024-02-08 14:53:34 +0100

    Create Kid entity

---
+++ Accounting_EF_Base/Entities/Kid.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Accounting_EF_Base.Entities
+{
+    public class Kid
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Lastname { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime? EndDate { get; set; }
+        public DateTime? Birthdate { get; set; }
+
+
+        public virtual ICollection<SiblingRelationship>? Siblings { get; set; }
+    }
+}
