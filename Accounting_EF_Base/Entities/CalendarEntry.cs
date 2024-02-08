7765a1ff7e091f1efa6c814c1839790ef6a7b000
Sergio <gomez.c.sergio@outlook.com>
2024-02-08 14:55:29 +0100

    Create CalendarEntry entity

---
+++ Accounting_EF_Base/Entities/CalendarEntry.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Accounting_EF_Base.Entities
+{
+    public class CalendarEntry
+    {
+        [Key]
+        public int Id { get; set; }
+        public virtual Kid Kid { get; set; }
+        public DateTime Date { get; set; }
+        public string Data { get; set; }
+        public bool Validated { get; set; }
+        public virtual Price Price { get; set; }
+    }
+}
