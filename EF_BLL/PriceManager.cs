1246d20e1e2fe184897b90ae5d10d5eb605de3d1
Sergio <gomez.c.sergio@outlook.com>
2024-02-09 10:39:21 +0100

    Create PriceManager in BLL

---
+++ EF_BLL/PriceManager.cs
@@ -0,0 +1,67 @@
+﻿using Accounting_EF_DAL;
+using EF_DTO;
+
+namespace EF_BLL
+{
+    public class PriceManager
+    {
+        private readonly DALPrice _PriceManager;
+
+        public PriceManager(DALPrice priceManager)
+        {
+            _PriceManager = priceManager;
+        }
+
+
+        /// <summary>
+        /// Create a new Price
+        /// </summary>
+        /// <param name="price">Price to be added to the database</param>
+        public void AddPrice(Price price)
+        {
+            _PriceManager.AddPrice(price);
+        }
+
+
+        /// <summary>
+        /// Get all the prices from the database, even those that are discontinued
+        /// </summary>
+        /// <returns>List of Price</returns>
+        public List<Price> GetPrices()
+        {
+            return _PriceManager.GetPrices();
+        }
+
+
+        /// <summary>
+        /// Get the currently active prices based on the start and end dates.
+        /// </summary>
+        /// <returns>List of Price</returns>
+        public List<Price> GetCurrentPrices()
+        {
+            List<Price> prices = GetPrices();
+           return prices.Where(p => (p.EndDate == null || p.EndDate > DateTime.Now) && p.StartDate <= DateTime.Now).ToList();
+        }
+
+
+        /// <summary>
+        /// Update a Price
+        /// </summary>
+        /// <param name="price">Price object to be updated</param>
+        public void UpdatePrice(Price price)
+        {
+            _PriceManager.UpdatePrice(price);
+        }
+
+
+
+        /// <summary>
+        /// Remove a Price from the database
+        /// </summary>
+        /// <param name="price">Price to be removed</param>
+        public void RemovePrice(Price price)
+        {
+            _PriceManager.RemovePrice(price);
+        }
+    }
+}
