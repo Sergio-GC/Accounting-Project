1c91c4206acced7b6dd00a640f3cc503d2713fed
Sergio <gomez.c.sergio@outlook.com>
2024-02-09 11:22:49 +0100

    Add SiblingRelationship DAL class

---
+++ Accounting_EF_DAL/DALSiblingRelationship.cs
@@ -0,0 +1,37 @@
+﻿using Accounting_EF_Base;
+using EF_DTO;
+
+namespace Accounting_EF_DAL
+{
+    public class DALSiblingRelationship : IDALSiblingRelationship
+    {
+        private readonly AccountingContext _ctx;
+
+        public DALSiblingRelationship(AccountingContext ctx)
+        {
+            _ctx = ctx;
+        }
+
+
+        /// <summary>
+        /// Create a new SiblingRelationship
+        /// </summary>
+        /// <param name="siblingRelationship">SiblingRelationship to be created</param>
+        public void CreateRelationship(SiblingRelationship siblingRelationship)
+        {
+            _ctx.Siblings.Add(siblingRelationship);
+            _ctx.SaveChanges();
+        }
+
+
+        /// <summary>
+        /// Remove SiblingRelationship from database
+        /// </summary>
+        /// <param name="relationship">SiblingRelationship to be deleted</param>
+        public void RemoveSiblingRelationship(SiblingRelationship relationship)
+        {
+            _ctx.Siblings.Remove(relationship);
+            _ctx.SaveChanges();
+        }
+    }
+}
---
+++ Accounting_EF_DAL/IDALSiblingRelationship.cs
@@ -0,0 +1,10 @@
+﻿using EF_DTO;
+
+namespace Accounting_EF_DAL
+{
+    public interface IDALSiblingRelationship
+    {
+        void CreateRelationship(SiblingRelationship siblingRelationship);
+        void RemoveSiblingRelationship(SiblingRelationship relationship);
+    }
+}
